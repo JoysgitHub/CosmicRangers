fed7dd054ad95420faaa2303719a962c37eedc85
Derek Peacock <derek.peacock@yahoo.com>
2022-12-04 10:48:32 +0000

    Refactored MyWorld single reponsibility

--- MyWorld.java
+++ MyWorld.java
@@ -10,43 +10,61 @@ public class MyWorld extends World
 {
     public static Counter heroLife = new Counter();
     public static Counter heroScore = new Counter();
+
+    public static final int SCREEN_WIDTH = 400;
+    public static final int SCREEN_HEIGHT = 600;
+    
     /**
      * Constructor for objects of class MyWorld.
+     * Create a new world with SCREEN_WIDTH X 
+     * SCREEN_HEIGHT cells with a cell size of 1X1 pixels.
      * 
      */
-    
     public MyWorld()
     {    
-        // Create a new world with 400x600 cells with a cell size of 1x1 pixels.
-        
+        super(SCREEN_WIDTH, SCREEN_HEIGHT, 1); 
 
-        super(400, 600, 1); 
-        heroLife.setValue(5);
-        heroScore.setValue(0);
+        addHero();
+        addEnemies();
         
+        setupScore();
+        setupLives();
+    }
+
+    private void addHero()
+    {
         addObject(new Hero(), 200, 560);
-        addObject(heroLife, 40, 590);
-        addObject(heroScore, 360, 590);
-        
+    }
+    
+    private void addEnemies()
+    {
         addObject(new Enemy(), 100, 20);
         addObject(new Enemy(), 200, 60);
         addObject(new Enemy(), 300, 20); 
         
-        for(int i=0;i<3;i++)
+        for(int i=0; i < 3; i++)
         {
            addObject(new Enemy(), 200,300); 
         }
-    
-        //
-        
         
         addObject(new Enemy(), 100, 20);
         addObject(new Enemy(), 200, 60);
         addObject(new Enemy(), 300, 20);
-        
     
         addObject(new Enemy(), 100, 130);
         addObject(new Enemy(), 200, 130);
         addObject(new Enemy(), 300, 130);  
+    }
+    
+    private void setupScore()
+    {
+        heroScore.setValue(0);
+        addObject(heroScore, 360, 590);
+    }
+    
+    private void setupLives()
+    {
+        heroLife.setValue(5);
+        addObject(heroLife, 40, 590);
     }
 }
