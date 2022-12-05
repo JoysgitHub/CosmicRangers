5bc4283ac3a1a67fa1cce61ac6c304d3b2b683a1
Keegan De souza <104536877+SPIKE445@users.noreply.github.com>
2022-12-05 22:56:06 +0000

    My world class refactored

--- MyWorld.java
+++ MyWorld.java
@@ -11,10 +11,15 @@ public class MyWorld extends World
     public static Counter heroLife = new Counter();
     public static Counter heroScore = new Counter();
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
     public static final int SCREEN_WIDTH = 600;
     public static final int SCREEN_HEIGHT = 800;
     
+=======
+    public static final int SCREEN_WIDTH = 500;
+    public static final int SCREEN_HEIGHT = 600;
+>>>>>>> Stashed changes
 =======
     public static final int SCREEN_WIDTH = 500;
     public static final int SCREEN_HEIGHT = 600;
@@ -27,6 +32,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
         super(SCREEN_WIDTH, SCREEN_HEIGHT, 1); 
 
@@ -67,12 +73,15 @@ public class MyWorld extends World
         addObject(new Enemy(), 200, 60);
         addObject(new Enemy(), 300, 20);
 =======
+=======
+>>>>>>> Stashed changes
         // Create a new world with 400x600 cells with a cell size of 1x1 pixels.
         super(SCREEN_WIDTH,SCREEN_HEIGHT, 1); 
         addScore();
         addLife();
         addHero();
     }
+<<<<<<< Updated upstream
     
       private void addLife()
     {
@@ -100,6 +109,20 @@ public class MyWorld extends World
         addObject(heroLife, 40, 590);
     }
 =======
+=======
+    
+      private void addLife()
+    {
+        heroLife.setValue(10);
+        addObject(heroLife, 60, 590);
+    }
+    
+    private void addScore()
+    {
+        heroScore.setValue(0);
+        addObject(heroScore, 450, 590);
+    }
+>>>>>>> Stashed changes
 
     private void addHero()
     {
@@ -118,5 +141,8 @@ public class MyWorld extends World
          addObject(new Enemy(), Greenfoot.getRandomNumber(480),0);
     }
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 }
