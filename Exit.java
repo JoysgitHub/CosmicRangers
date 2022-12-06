d40ee6f7a1f20c9aa903ad6bea5301dade173cd1
MohammadQasim03 <114746600+MohammadQasim03@users.noreply.github.com>
2022-12-06 15:15:12 +0000

    Added buttons Restart, Exit, designed buttons and annotations

---
+++ Exit.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Exist here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Exit extends Actor
+{
+    /**
+     * Act - do whatever the Exist wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     public Exit()
+    {   
+        //This sets the bullet size
+        getImage().scale(150,80);
+    }
+    public void act()
+    {
+    if(Greenfoot.mousePressed(this))
+    //This stops the game.  
+        {
+            Greenfoot.stop();
+        }
+    }
+}
--- WinWorld.java
+++ WinWorld.java
@@ -16,5 +16,9 @@ public class WinWorld extends World
     {    
         // Creates a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500,600, 1); 
+        //This line of code adds the restart button to the Win world menu.
+        addObject(new RestartButton(),250,270);
+        // This adds the Exit button to the "WinWorld"
+        addObject(new Exit(),250,380);
     }
 }
